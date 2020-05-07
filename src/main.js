49993ded49c75ca95bb86c1c5bbe04d1d8a33586
braisv <brais.vidal.rodriguez@gmail.com>
2020-05-07 12:50:12 +0200

    installed axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import BootstrapVue from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.use(VueAxios, axios);
+
+axios.defaults.baseURL = "http://localhost:3000/api";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
