144ed1bd104d776465047223900923ef964e0b3b
braisv <brais.vidal.rodriguez@gmail.com>
2020-05-08 19:32:04 +0200

    added notes route&view

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,17 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/notes',
+    name: 'Notes',
+    component: () => import('../views/Notes.vue')
   }
 ]
 
---
+++ src/views/Notes.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>NOTES</h1>
+    </div>
+</template>
